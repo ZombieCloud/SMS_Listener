c7ce4b52c11d0b71252fb5ced8102717037e1b21
ZombieCloud <konstantin.gladky@gmail.com>
2021-07-27 00:47:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sms_listener/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sms_listener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = SmsReceiver.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendMessage(View view) {
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Privettt");
+
+        Log.d(TAG, "PRIVETTT");
+    }
+
+
+}
